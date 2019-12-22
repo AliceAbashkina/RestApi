3cbbc42e75b3a0c1a91915d0e507a49368fd8699
AliceInTheWonderland <58621587+AliceInTheWonderland@users.noreply.github.com>
2019-12-22 15:06:04 +0600

    Update app.py
    Class for result

--- app.py
+++ app.py
@@ -14,6 +14,21 @@ db.init_app(app)
 migrate = Migrate()
 migrate.init_app(app,db)
 ma = Marshmallow(app)
+class Result(db.Model):
+    __tablename__ = 'results'
+
+    id = db.Column(db.BIGINT, primary_key=True)
+    name = db.Column(db.String(64))
+    email = db.Column(db.String(64))
+    password = db.Column(db.String(64))
+    
+    def __init__(self, name, email, password):
+        self.name = name
+        self.email = email
+        self.password = password
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)   
 
 if __name__ == '__main__':
     app.run()
