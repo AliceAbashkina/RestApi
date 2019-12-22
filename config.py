df25eaa7f87b8bb92227b6a7f0a9505b6be5c4fc
AliceInTheWonderland <58621587+AliceInTheWonderland@users.noreply.github.com>
2019-12-22 15:04:26 +0600

    Update config.py
    Add connect to database

--- config.py
+++ config.py
@@ -4,3 +4,6 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'this-really-needs-to-be-changed'
+    #db_url = "postgresql://Salem:1qazxsw23edc@localhost:5432/python_res" 
+    #SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://postgres:1@localhost:5432/Results"
