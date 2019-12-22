81b3995ef61f56d231570e09e7b6aa21fbd4dc0b
AliceInTheWonderland <58621587+AliceInTheWonderland@users.noreply.github.com>
2019-12-22 14:41:18 +0600

    Update
    add SqlAlchemy, migrate features from it

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+from flask import Flask, jsonify, request 
+from flask_sqlalchemy import SQLAlchemy
 import os
-from flask import Flask
+from sqlalchemy.dialects.postgresql import JSON, BIGINT, VARCHAR
 
+app = Flask(__name__)
+app.config.from_object(DevelopmentConfig)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+db.init_app(app)
 app = Flask(__name__)
 if __name__ == '__main__':
     app.run()
